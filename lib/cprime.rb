3f89665a483056b6a83690a3e9b5d2f0d3ed2299
ChrisBentley <c.j.bentley90@gmail.com>
2017-01-06 00:58:23 +0000

    Added more tests of is_prime?
    Separated out the prime number checking into separte tests and added a speed test
    to make sure the method will be faster when checking against primes that are already known

--- lib/cprime.rb
+++ lib/cprime.rb
@@ -6,7 +6,7 @@ class Cprime
 	def is_prime?(n)
 		return false if n < 2
 		return true if @known_primes.include?(n)
-		return false if @known_primes.length != 0 and @known_primes[-1] > n
+		# return false if @known_primes.length != 0 and @known_primes[-1] > n
 
 		(2...n).each do |i|
 			return false if n % i == 0
@@ -14,5 +14,9 @@ class Cprime
 
 		@known_primes.push(n)
 		return true
+	end
+
+	def generate_primes(n)
+		
 	end
 end
--- spec/cprime_spec.rb
+++ spec/cprime_spec.rb
@@ -1,11 +1,41 @@
 require 'cprime'
+require 'benchmark'
 
 RSpec.describe Cprime, ".is_prime?" do
-	context "with no known primes," do
-		it "figures out if a number is prime or not" do
+	context "with no known primes" do
+
+		it "returns true for a prime number" do
 			cprime = Cprime.new
 			expect(cprime.is_prime?(7)).to eq true
+		end
+
+		it "returns false for a non-prime number" do
+			cprime = Cprime.new
 			expect(cprime.is_prime?(4)).to eq false
+		end
+
+		it "returns false for a number less than 2" do
+			cprime = Cprime.new
+			expect(cprime.is_prime?(1)).to eq false
+			expect(cprime.is_prime?(-10)).to eq false
+		end
+	end
+
+	context "with some known primes" do
+		it "returns faster than without" do
+			cprime = Cprime.new
+
+			beginning_time = Time.now
+			cprime.is_prime?(97)
+			end_time = Time.now
+			first_run_time = (end_time - beginning_time)*1000
+
+			beginning_time = Time.now
+			cprime.is_prime?(97)
+			end_time = Time.now
+			second_run_time = (end_time - beginning_time)*1000
+
+			expect(first_run_time).to be > second_run_time
 		end
 	end
 end
