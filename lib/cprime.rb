afc55531b3c292ddc659fec2639e4f158c5de06f
ChrisBentley <c.j.bentley90@gmail.com>
2017-01-06 00:23:38 +0000

    implemented basic checking of number being prime

--- lib/cprime.rb
+++ lib/cprime.rb
@@ -1,7 +1,18 @@
 class Cprime
-	# attr_reader :known_primes
+	def initialize
+		@known_primes = []
+	end
 
 	def is_prime?(n)
+		return false if n < 2
+		return true if @known_primes.include?(n)
+		return false if @known_primes.length != 0 and @known_primes[-1] > n
 
+		(2...n).each do |i|
+			return false if n % i == 0
+		end
+
+		@known_primes.push(n)
+		return true
 	end
 end
--- spec/cprime_spec.rb
+++ spec/cprime_spec.rb
@@ -1,10 +1,11 @@
 require 'cprime'
 
-RSpec.describe Cprime, "#is_prime?" do
-	context "with no known primes" do
+RSpec.describe Cprime, ".is_prime?" do
+	context "with no known primes," do
 		it "figures out if a number is prime or not" do
 			cprime = Cprime.new
 			expect(cprime.is_prime?(7)).to eq true
+			expect(cprime.is_prime?(4)).to eq false
 		end
 	end
 end
